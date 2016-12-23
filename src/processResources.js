a0d240f0fedf9c587efc378da46686cdc106be5d
David Ash <green.neon@gmail.com>
2016-12-23 10:40:08 -0700

    Processing resources now sorts by order and combines adjacent people

--- src/processResources.js
+++ src/processResources.js
@@ -1,5 +1,45 @@
-const processResources = (resources) => {
-  return resources;
+import { fromJS } from 'immutable';
+import { TYPE_PERSON } from './constants';
+
+const emptyMemo = {
+  type: TYPE_PERSON,
+  order: null,
+  people: [],
+};
+
+const flushPeople = reduction => {
+  if(reduction.getIn(['memo', 'people']).size < 1) {
+    return reduction;
+  }
+
+  return reduction.update('output', output => (
+    output.push(reduction.get('memo'))
+  )).set('memo', fromJS(emptyMemo));
+};
+
+const personReducer = (reduction, resource) => {
+  switch(resource.get('type')) {
+    case TYPE_PERSON:
+      return reduction.update('memo', memo => (
+        memo.set('order', resource.get('order'))
+          .update('people', people => people.push(resource.get('name')))
+      ));
+    default:
+      return flushPeople(reduction).update('output', output => (
+        output.push(resource)
+      ));
+  }
+}
+
+const processResources = resources => {
+  const reduction = fromJS(resources)
+    .sort((a, b) => a.get('order') - b.get('order'))
+    .reduce(personReducer, fromJS({
+      memo: emptyMemo,
+      output: []}
+    ));
+
+  return flushPeople(reduction).get('output').toJS();
 };
 
 export default processResources;
--- src/processResources.test.js
+++ src/processResources.test.js
@@ -4,3 +4,45 @@ import resources from './resources';
 it('processes without crashing', () => {
   processResources(resources);
 });
+
+it('processes correctly (data 1)', () => {
+  expect(processResources(resources)).toEqual([
+		{
+			"type": "Person",
+			"order": 1,
+			"people": [
+				"Brian"
+			]
+		},
+		{
+			"type": "Place",
+			"order": 2,
+			"name": "Ohio"
+		},
+		{
+			"type": "Place",
+			"order": 12,
+			"name": "Ohio"
+		},
+		{
+			"type": "Person",
+			"order": 19,
+			"people": [
+				"Sarah",
+				"Eric"
+			]
+		},
+		{
+			"type": "Place",
+			"order": 20,
+			"name": "Home"
+		},
+		{
+			"type": "Person",
+			"order": 199,
+			"people": [
+				"Sam"
+			]
+		}
+	]);
+});
